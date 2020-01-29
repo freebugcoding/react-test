cabc9d58a7c6cf4ef35b05a7e597ddf042a0c44f
kbennis <kbennis@octo.com>
2020-01-29 13:27:02 +0100

    sry

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
+  const baseUrl = '/';
+  const [input, setInput] = useState(null)
+  const [messages, setMessages] = useState([]);
+
+  const saveMessage= (input) => {axios.post(baseUrl, {
+    data: input
+  }).then((res)=> setMessages([...messages, res.data.data]))}
+
+  const getMessages = () => {axios.get("http://localhost:8080/").then((res)=> {console.log(res.data); setMessages([...res.data.map(data => data.data)]);})}
+
+  useEffect(() => {
+    getMessages();
+  }, [])
+
+
+  return (<div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={input} onChange={ e => setInput(e.target.value) } />
+      <button onClick={()=> saveMessage(input)}>save</button>
+    </div>
+    <div>
+      <table>
+        {messages.map(m => (<tr><th>{m}</th></tr>))}
+      </table>
+    </div>
     </div>
   );
 }
